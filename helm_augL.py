2aff2499a7b13947cb53fe2ad436426576e76aa3
Colin Cotter <colin.cotter@imperial.ac.uk>
2021-07-06 16:47:20 +0100

    bring in the PC from asQ

--- helm_augL.py
+++ helm_augL.py
@@ -7,221 +7,6 @@ from scipy.fft import fft, ifft
 import petsc4py.PETSc as PETSc
 PETSc.Sys.popErrorHandler()
 
-class IPHelmholtzPC(fd.PCBase):
-
-    needs_python_pmat = True
-
-    def initialize(self, pc):
-        from firedrake import TrialFunction, TestFunction, dx, assemble, inner, parameters
-        prefix = pc.getOptionsPrefix()
-        options_prefix = prefix + "Hp_"
-        # we assume P has things stuffed inside of it
-        _, P = pc.getOperators()
-        context = P.getPythonContext()
-
-        test, trial = context.a.arguments()
-
-        if test.function_space() != trial.function_space():
-            raise ValueError("MassInvPC only makes sense if test and trial space are the same")
-
-        V = test.function_space()
-        mesh = V.mesh()
-
-        # Input/Output wrapper Functions
-        self.xf = fd.Function(V)  # input
-        self.yf = fd.Function(V)  # output
-
-        mu = context.appctx.get("mu", 1.0)
-        D1r = context.appctx.get("D1r", None)
-        D1i = context.appctx.get("D1i", None)
-        sr = context.appctx.get("sr", None)
-        si = context.appctx.get("si", None)
-        gamma = context.appctx.get("gamma", None)
-
-        self.D1r = D1r
-        self.D1i = D1i
-        self.sr = sr
-        self.si = si
-
-        u = fd.TrialFunction(V)
-        v = fd.TestFunction(V)
-
-        #mass solve
-        m = fd.inner(u,v)*fd.dx
-        sp = {
-            "ksp_type":"preonly",
-            "pc_type": "bjacobi",
-            "sub_pc_type":"ilu"
-        }
-        self.mSolver = fd.LinearSolver(assemble(m),
-                                       solver_parameters=sp)
-        vr = v[0]
-        vi = v[1]
-        ur = u[0]
-        ui = u[1]
-        xr = self.xf[0]
-        xi = self.xf[1]
-
-        def get_laplace(q,phi):
-            h = fd.avg(fd.CellVolume(mesh))/fd.FacetArea(mesh)
-            eta = fd.Constant(10.)
-            mu = eta/h
-            n = fd.FacetNormal(mesh)
-            ad = (- inner(2 * fd.avg(phi*n),
-                          fd.avg(fd.grad(q)))
-                  - inner(fd.avg(fd.grad(phi)),
-                          2 * fd.avg(q*n))
-                  + mu * inner(2 * fd.avg(phi*n),
-                               2 * fd.avg(q*n))) * fd.dS
-            ad += inner(fd.grad(q), fd.grad(phi)) * fd.dx
-            return ad
-
-        D2u_r = D2r*ur - D2i*ui
-        D2u_i = D2i*ur + D2r*ui
-        su_r = sr*ur - si*ui
-        su_i = si*ur + sr*ui
-
-        self.D2r = D2r
-        self.D2i = D2i
-        self.sr = sr
-        self.si = si
-
-        a = vr * D2u_r * dx + get_laplace(vr, su_r)
-        a += vi * D2u_i * dx + get_laplace(vi, su_i)
-        L = get_laplace(xr, vr/gamma) + get_laplace(xi, vi/gamma)
-
-        Hprob = fd.LinearVariationalProblem(a, L, self.yf,
-                                            constant_jacobian=False)
-        Hparameters = {
-            "ksp_type":"preonly",
-            "pc_type": "lu",
-            "pc_factor_mat_solver_type": "mumps"
-        }
-        nullspace = fd.VectorSpaceBasis(constant=True)
-        self.solver = fd.LinearVariationalSolver(Hprob,
-                                                 #nullspace = nullspace,
-                                                 solver_parameters
-                                                 = Hparameters)
-
-    def update(self, pc):
-        pass
-
-    def apply(self, pc, x, y):
-        # copy petsc vec into Function
-        with self.xf.dat.vec_wo as v:
-            x.copy(v)
-
-        #solve mass matrix on xf, put solution in yf
-        self.mSolver.solve(self.yf, self.xf)
-        #copy into yf for RHS of Helmholtz operator
-        self.xf.assign(self.yf)
-        #Do Helmholtz solve
-        self.solver.solve()
-
-        # copy Function into petsc vec
-        with self.yf.dat.vec_ro as v:
-            v.copy(y)
-
-    def applyTranspose(self, pc, x, y):
-        raise NotImplementedError
-
-class MixedHelmholtzPC(fd.PCBase):
-    #rather tautological
-    needs_python_pmat = True
-
-    def initialize(self, pc):
-        from firedrake import TrialFunction, TestFunction, dx, assemble, inner, parameters
-        prefix = pc.getOptionsPrefix()
-        options_prefix = prefix + "Hp_"
-        # we assume P has things stuffed inside of it
-        _, P = pc.getOperators()
-        context = P.getPythonContext()
-
-        test, trial = context.a.arguments()
-
-        if test.function_space() != trial.function_space():
-            raise ValueError("MassInvPC only makes sense if test and trial space are the same")
-
-        Q = test.function_space()
-        mesh = Q.mesh()
-
-        # Input/Output wrapper Functions
-        self.xf = fd.Function(V)  # input
-        self.yf = fd.Function(V)  # output
-
-        D1r = context.appctx.get("D1r", None)
-        D1i = context.appctx.get("D1i", None)
-        sr = context.appctx.get("sr", None)
-        si = context.appctx.get("si", None)
-        gamma = context.appctx.get("gamma", None)
-        W = context.appctx.get("W", None)
-        V = context.appctx.get("V", None)
-
-        self.D1r = D1r
-        self.D1i = D1i
-        self.sr = sr
-        self.si = si
-
-        p = fd.TrialFunction(Q)
-        q = fd.TestFunction(Q)
-
-        #mass solve
-        m = fd.inner(p,q)*fd.dx
-        sp = {
-            "ksp_type":"preonly",
-            "pc_type": "bjacobi",
-            "sub_pc_type":"ilu"
-        }
-        self.mSolver = fd.LinearSolver(assemble(m),
-                                       solver_parameters=sp)
-
-        #Applying the gradient
-        v = fd.TrialFunction(V)
-        u = fd.TestFunction(V)
-        ur = u[0, :]
-        ui = u[1, :]
-        vr = v[0, :]
-        vi = v[1, :]
-        
-        a = fd.inner(v,u)*fd.dx 
-        L = fd.div(vr)*
-        
-        Hprob = fd.LinearVariationalProblem(a, L, self.yf,
-                                            constant_jacobian=False)
-        Hparameters = {
-            "ksp_type":"preonly",
-            "pc_type": "lu",
-            "pc_factor_mat_solver_type": "mumps"
-        }
-        nullspace = fd.VectorSpaceBasis(constant=True)
-        self.solver = fd.LinearVariationalSolver(Hprob,
-                                                 #nullspace = nullspace,
-                                                 solver_parameters
-                                                 = Hparameters)
-
-    def update(self, pc):
-        pass
-
-    def apply(self, pc, x, y):
-        # copy petsc vec into Function
-        with self.xf.dat.vec_wo as v:
-            x.copy(v)
-
-        #solve mass matrix on xf, put solution in yf
-        self.mSolver.solve(self.yf, self.xf)
-        #copy into yf for RHS of Helmholtz operator
-        self.xf.assign(self.yf)
-        #Do Helmholtz solve
-        self.solver.solve()
-
-        # copy Function into petsc vec
-        with self.yf.dat.vec_ro as v:
-            v.copy(y)
-
-    def applyTranspose(self, pc, x, y):
-        raise NotImplementedError
-
-
 n = 20
 mesh = fd.PeriodicUnitSquareMesh(n, n)
 degree = 1
@@ -296,8 +81,6 @@ fpi = fd.Function(Q0).interpolate(fd.sin(fd.exp(2*p1-p2)))
 one = fd.Function(Q0).assign(1.)
 fpr -= fd.assemble(fpr*fd.dx)/fd.assemble(one*fd.dx)
 fpi -= fd.assemble(fpi*fd.dx)/fd.assemble(one*fd.dx)
-F = fd.inner(vr, fur)*fd.dx + fd.inner(vi, fui)*fd.dx
-F += (fpr*qr + fpi*qi)*fd.dx
 D1xu_r = D1r*ur - D1i*ui
 D2xu_r = D2r*ur - D2i*ui
 D1xp_r = D1r*pr - D1i*pi
@@ -317,13 +100,15 @@ a = (
         + fd.div(vi)*(D1xp_i + fd.div(D2xu_i))
     )
     )*fd.dx
-
+F = fd.inner(vr, fur)*fd.dx + fd.inner(vi, fui)*fd.dx
+F += (fpr*qr + fpi*qi)*fd.dx
+F += gamma*(fd.div(vr)*fpr + fd.div(vi)*fpi)*fd.dx
 w = fd.Function(W)
 prob = fd.LinearVariationalProblem(a, F, w, constant_jacobian=False)
 
 diag_parameters = {
     "mat_type": "matfree",
-    "ksp_type": "preonly",
+    "ksp_type": "fgmres",
     "ksp_monitor": None,
     "ksp_atol": 1.0e-6,
     "pc_type": "fieldsplit",
@@ -336,10 +121,10 @@ diag_parameters = {
 bottomright = {
     "ksp_type": "gmres",
     "ksp_gmres_modifiedgramschmidt": None,
-    "ksp_max_it": 50,
     "ksp_converged_reason": None,
+    "ksp_max_it": 3,
     "pc_type": "python",
-    "pc_python_type": "__main__.IPHelmholtzPC"
+    "pc_python_type": "asQ.HelmholtzPC"
 }
 
 diag_parameters["fieldsplit_1"] = bottomright
@@ -360,8 +145,8 @@ si = fd.Constant(0.)
 v_basis = fd.VectorSpaceBasis(constant=True)
 nullspace = fd.MixedVectorSpaceBasis(W, [W.sub(0), v_basis])
 solver = fd.LinearVariationalSolver(prob, solver_parameters=diag_parameters,
-                                    #nullspace=nullspace,
-                                    appctx={"D1r": D1r, "D1i": D1i,
+                                    nullspace=nullspace,
+                                    appctx={"D2r": D2r, "D2i": D2i,
                                             "sr": sr, "si": si,
                                             "gamma": gamma,
                                             "W": W, "V": V})
